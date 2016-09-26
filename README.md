e7939049ea2964828abe548546035755dc4cb97e
jebstern <jesper.skand@gmail.com>
2016-09-26 19:08:58 +0300

    readme update, new HelpFragment, new icons

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # PetrolBook
-A demo Android app intended to log refuels
 
-readme will be updated soon...
+This is a demo app that I'm developing to learn more about popular technologies in Android development.
+Firebase is good candidate for storing data and syncing it between users, but I wanted to use my own REST API.
+
+
+Implemented stuff (still more to come):
+- RxJava
+- RetroFit (using GSON)
+- Realm
+
+
+Not yet implemented:
+- Butter knife
+- MVP pattern
+- Custom animations (if needed)
+- Testing
---
+++ app/src/main/java/com/jebstern/petrolbook/fragments/HelpFragment.java
@@ -0,0 +1,26 @@
+package com.jebstern.petrolbook.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jebstern.petrolbook.R;
+
+public class HelpFragment extends Fragment {
+
+
+    public HelpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+
+}
